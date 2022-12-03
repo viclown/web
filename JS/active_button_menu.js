83e9e3679e72fb99f2a7da1d73dc5ee64b382e06
viclown <76227700+viclown@users.noreply.github.com>
2022-12-03 23:45:03 +0300

    fix: last fix menu button

--- JS/active_button_menu.js
+++ JS/active_button_menu.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('nav a').forEach(link => {
     if (link.href.includes(window.location.pathname)){
-        if (link.href.endsWith('web')) {
-            window.location.href = link.href + '/index.html';
+        if (window.location.pathname.endsWith('web/')) {
+            window.location.pathname = window.location.pathname + 'index.html';
         }
         else link.classList.add('active')
     }
