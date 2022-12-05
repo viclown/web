77266572593614ab167613f039a4c550e4b9c9a1
viclown <76227700+viclown@users.noreply.github.com>
2022-12-05 15:06:56 +0300

    fix: active menu buttons

--- JS/active_button_menu.js
+++ JS/active_button_menu.js
@@ -1,8 +1,5 @@
 document.querySelectorAll('nav a').forEach(link => {
-    if (link.href.includes(window.location.pathname)){
-        if (window.location.pathname.endsWith('web/')) {
-            window.location.pathname = window.location.pathname + 'index.html';
-        }
-        else link.classList.add('active')
+    if (link.href === window.location.href) {
+        link.classList.add('active')
     }
 })
